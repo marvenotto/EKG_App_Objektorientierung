8ec0a339be1d2d3ddb996b7b772bf51d0ce7f5cb
Cedric Rissi <cedric.rissi2003@gmail.com>
2026-05-12 14:18:07 +0200

    stream_lit select Box erstellen

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # EKG_App_Steamlit
 
+
+To start the Prjekt type in Terminal:
+
+pdm run streamlit run main.py
+
+
+
+You will get ah small info for the Local Werbsite that looks like that:
+
+Local URL: http://localhost:8501
+  Network URL: http://10.55.15.193:8501
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import streamlit as st
 
+
+
+
 # Eine Überschrift der ersten Ebene
 st.write("# EKG APP")
 
 # Eine Überschrift der zweiten Ebene
 st.write("## Versuchsperson auswählen")
 
+
+
 # Eine Auswahlbox
 current_user = st.selectbox(
     'Versuchsperson',
-    options = ["Nutzer1", "Nutzer2"], key="sbVersuchsperson")
+    options = ["Cedi Tyson", "Otto Otto", "Max"], key="sbVersuchsperson")
+
+# Ein Button
+if st.button("Daten anzeigen", key="btnDatenAnzeigen"):
+    st.write("Daten von " + current_user)
+    #
