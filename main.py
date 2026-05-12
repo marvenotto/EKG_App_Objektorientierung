61a6c962a3405e6c4755c8a6de3e6cd56ca1cddc
Cedric Rissi <cedric.rissi2003@gmail.com>
2026-05-12 13:49:10 +0200

    streamlit installieren und add
    kleiner debug code zum streamlit testen

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def main():
+    st.title("a, Streamlit!")
+    st.write("This is a simple Streamlit app.")
+    st.write("You can add more components and functionality here.")
+
+if __name__ == "__main__":
+    main()
