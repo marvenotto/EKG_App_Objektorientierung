f278f3d384d9ce75351eb5b6fbc43c1a50de422a
Cedric Rissi <cedric.rissi2003@gmail.com>
2026-05-19 11:46:27 +0200

    Anpassungen und merges mit neuen Dateien

--- main.py
+++ main.py
@@ -1,35 +1,8 @@
 import streamlit as st
 from source.read_data import get_person_list, get_person_dict, get_person_picture
-from source.read_data import get_person_list, get_person_dict, get_person_picture
 
-
-
-st.write("# EKG APP")
-st.write("## Versuchsperson auswählen")
-
-# Initialisiere Session State Variablen
-if 'current_user_data' not in st.session_state:
-    st.session_state.current_user_data = None
-
-person_list = get_person_list()
-current_user = st.selectbox('Versuchsperson', options=person_list, key="sbVersuchsperson")
-
-if st.button("Daten anzeigen", key="btnDatenAnzeigen"):
-    # Daten einmalig beim Klick laden und im State speichern
-    all_persons = get_person_dict()
-    if current_user in all_persons:
-        st.session_state.current_user_data = all_persons[current_user]
-        st.session_state.picture_path = get_person_picture(current_user)
-        print(get_person_picture(current_user)) # Debug-Ausgabe
-
-# Anzeige (bleibt bestehen, solange etwas im State ist)
-if st.session_state.current_user_data:
-    user = st.session_state.current_user_data
-    st.write(f"### Daten von {current_user}")
-    st.write(f"**Alter:** {user['alter']}")
-    st.image(st.session_state.picture_path, width=200)
-
-
+from source.read_pandas import read_my_csv
+from source.read_pandas import make_plot
 
 st.write("# EKG APP")
 st.write("## Versuchsperson auswählen")
@@ -39,38 +12,37 @@ if 'current_user_data' not in st.session_state:
     st.session_state.current_user_data = None
 
 person_list = get_person_list()
-current_user = st.selectbox('Versuchsperson', options=person_list, key="sbVersuchsperson")
+
+current_user = st.selectbox(
+    'Versuchsperson',
+    options=person_list,
+    key="sbVersuchsperson"
+)
 
 if st.button("Daten anzeigen", key="btnDatenAnzeigen"):
-    # Daten einmalig beim Klick laden und im State speichern
+
     all_persons = get_person_dict()
+
     if current_user in all_persons:
         st.session_state.current_user_data = all_persons[current_user]
         st.session_state.picture_path = get_person_picture(current_user)
-        print(get_person_picture(current_user)) # Debug-Ausgabe
 
-# Anzeige (bleibt bestehen, solange etwas im State ist)
+# Anzeige
 if st.session_state.current_user_data:
+
     user = st.session_state.current_user_data
+
     st.write(f"### Daten von {current_user}")
     st.write(f"**Alter:** {user['alter']}")
+
     st.image(st.session_state.picture_path, width=200)
 
-
-    import streamlit as st
-from read_pandas import read_my_csv
-from read_pandas import make_plot
-
-
-# Wo startet sie Zeitreihe
-# Wo endet sich
-# Was ist die Maximale und Minimale Spannung
-# Grafik
+# Tabs
 tab1, tab2 = st.tabs(["EKG-Data", "Power-Data"])
 
 with tab1:
+
     st.header("EKG-Data")
-    st.write("# My Plot")
 
     df = read_my_csv()
     fig = make_plot(df)
@@ -78,4 +50,5 @@ with tab1:
     st.plotly_chart(fig)
 
 with tab2:
+
     st.header("Power-Data")
